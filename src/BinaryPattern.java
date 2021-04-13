08d836f66cc9de2c44b08ad3e1fd9048c90fc007
udeshgn08 <forwritingforwriting@gmail.com>
2021-04-13 13:54:43 +0200

    javacodes > Print 0's and 1's Division

---
+++ src/BinaryPattern.java
@@ -0,0 +1,22 @@
+/*
+ * Print line of binary pattern O's and 1's. 
+ */
+
+
+public class BinaryPattern {
+
+	public static void main(String[] args) { 
+		
+	    System.out.println(" 0 0 0 0\n" + " 0 0 0 1");
+		System.out.println(" 0 0 1 0\n" + " 0 0 1" + " " + "1");
+		System.out.println(" 0" + " " +"1 "+"0 0"  ); 
+		System.out.println(" 0 1 0 1"); 
+		System.out.println(" 0 1 1 0\n" + " 0 1 1 1");
+		System.out.println("___________________\n\n" + "16 cols * 4 rows\n" + "___________________\n");
+		System.out.println(" 1 0 0 0");
+		System.out.println(" 1" + " " + "0" + " " + "0 " + 1 );
+		System.out.println(" 1 0 1 0");
+		System.out.println(" 1 0 1 1"); 
+		System.out.println(" 1 1 0 0\n" + " 1 1 " + "0 " + "1\n" + " 1 1 1 0\n" + " 1 1 1 1");
+	}
+}
---
+++ src/Division.java
@@ -0,0 +1,16 @@
+/* 
+ * What is the remainder and quotient of 10 divisible by 2  
+ * 
+ */
+public class Division {
+
+	public static void main(String[] args) {
+		// Division and modulus operator 
+		int dividend=10, divisor=2; 
+		int remainder = dividend%divisor, quotient = dividend/divisor; 
+		
+		System.out.println("\n Division and Modulus of 10 divisible by 2 \n " + "Remainder of an int = " +remainder);
+        System.out.println(" Quotient = " +quotient);
+	}
+
+}

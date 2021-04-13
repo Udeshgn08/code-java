d05cbf0294f7931374e22d5d3057b0a62eba9668
udeshgn08 <forwritingforwriting@gmail.com>
2021-04-13 12:35:59 +0200

    String to print my name

---
+++ src/Myname.java
@@ -0,0 +1,11 @@
+
+public class Myname {
+
+	public static void main(String[] args) {
+		// Print my name
+		String first_name = "Udesh", surname = "Ganesan";
+		System.out.println("Hi I'm " + first_name + surname + "\nhow may I help you?");
+
+	}
+
+}

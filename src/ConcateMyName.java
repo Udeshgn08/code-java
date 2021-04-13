d3ba5aa190daee9cc508e11ef322fa8b3b319f66
udeshgn08 <forwritingforwriting@gmail.com>
2021-04-13 13:29:17 +0200

    javacode > Concatenate my name with a message

---
+++ src/ConcateMyName.java
@@ -0,0 +1,11 @@
+
+public class ConcateMyName {
+
+	public static void main(String[] args) {
+		// Print my name
+		String first_name = "Udeshkumar", surname = "Ganesan";
+		System.out.println("Hi I'm " + first_name + surname + "\nhow may I help you?");
+
+	}
+
+}
